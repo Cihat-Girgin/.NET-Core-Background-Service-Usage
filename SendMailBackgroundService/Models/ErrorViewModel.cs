2450f148d5813d93b12ad2d467edf70e02b50b5c
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-15 22:40:49 +0300

    Project created

---
+++ SendMailBackgroundService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SendMailBackgroundService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
