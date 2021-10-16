292f2e03d3a6766e32465739f1b77534f6e7f7b6
Cihat Girgin <cihatgirgin1@gmail.com>
2021-10-16 11:49:07 +0300

    Create PeriodicSendMailService.cs

---
+++ SendMailBackgroundService/BackgroundServices/PeriodicSendMailService.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Net.Mail;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using SendMailBackgroundService.Models;
+
+namespace SendMailBackgroundService.BackgroundServices
+{
+    public class PeriodicSendMailService:BackgroundService
+    {
+        private readonly InMemoryContext _context;
+
+        public PeriodicSendMailService(InMemoryContext context)
+        {
+            _context = context;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+              SendMail();
+
+              return Task.CompletedTask;
+        }
+
+        private void SendMail()
+        {
+            var mailList = _context.UserMails.ToList();
+            
+            mailList.ForEach(mail =>
+            {
+                
+                
+            });
+            
+        }
+
+        private void sendmaill(string email)
+        {
+           
+        }
+    }
+}
